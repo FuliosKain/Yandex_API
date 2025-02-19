12035aa28b9fd09052a4f55cce687fd037d35917
Admin <arsenijcugajnov709@gmail.com>
2025-02-19 17:13:10 +0300

    sehseh

---
+++ find.py
@@ -0,0 +1,31 @@
+import requests
+from scale import scale
+from io import BytesIO
+
+
+def find_the_place(coords):
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+    geocoder_params = {"apikey": "8013b162-6b42-4997-9691-77b7074026e0",
+                       "geocode": coords,
+                       "format": "json"}
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+    json_response = response.json()
+    toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+    param = scale(toponym)
+    toponym_longitude, toponym_lattitude = param[0].split()
+
+    apikey = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
+
+    map_params = {
+        "ll": ",".join([toponym_longitude, toponym_lattitude]),
+        "spn": ",".join([str(param[1]), str(param[2])]),
+        "apikey": apikey
+    }
+
+    map_api_server = "https://static-maps.yandex.ru/v1"
+    response = requests.get(map_api_server, params=map_params)
+    image = BytesIO(response.content)
+    return image
+
+
+find_the_place(('39.605562 52.565400'))
